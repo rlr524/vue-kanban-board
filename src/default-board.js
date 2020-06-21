281c9daeaf77872c40834c3abbb93274491303ea
Rob Ranf <robranf@gmail.com>
2020-06-21 15:32:09 -0700

    first commit

---
+++ src/default-board.js
@@ -0,0 +1,58 @@
+import { uuid } from "./utils";
+
+export default {
+  name: "workshop",
+  columns: [
+    {
+      name: "todo",
+      tasks: [
+        {
+          description: "Need to look at Rila. She's so easy to look at.",
+          name: "Look at Rila",
+          id: uuid(),
+          userAssigned: null
+        },
+        {
+          description: "Feed Rila when she's hungry.",
+          name: "Feed Rila",
+          id: uuid(),
+          userAssigned: null
+        },
+        {
+          description: "Watch movies with Rila, probably Kurosawa.",
+          name: "Watch movies with Rila",
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: "in-progress",
+      tasks: [
+        {
+          description: "",
+          name: "first task",
+          id: uuid(),
+          userAssigned: null
+        },
+        {
+          description: "",
+          name: "second task",
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: "done",
+      tasks: [
+        {
+          description: "",
+          name: "first task",
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    }
+  ]
+};
